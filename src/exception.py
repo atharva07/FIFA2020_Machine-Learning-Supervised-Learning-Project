a70ad1764901f701aed63c4db457887d63a84a99
atharva07 <atharva.hiwase07@gmail.com>
2023-05-28 01:55:04 +0530

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,6 +1,6 @@
-import logging
 import sys
 import logging
+from src.logger import logging
 
 def error_message_details(error, error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
@@ -8,7 +8,7 @@ def error_message_details(error, error_detail:sys):
     error_message='Error occured in python script name [{0}] line number [{1}] error message [{2}]'.format(
         file_name,exc_tb.tb_lineno,str(error)
     )
-    return error_message    
+    return error_message
 
 class CustomException(Exception):
     def __init__(self, error_message, error_detail:sys):
@@ -17,5 +17,3 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message
-
-        
